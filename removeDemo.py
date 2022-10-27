e3e0cf9672a6e5590e6d294441d735c47e551f88
jsantore <jsantore@bridgew.edu>
2022-10-27 11:48:28 -0400

    use while to remove all

---
+++ removeDemo.py
@@ -0,0 +1,8 @@
+books = ["Python Crash Course", "Game Programming",
+         "Code Complete", "Clean Code","Python Crash Course",
+         "The Pragmatic Programmer", "Python Crash Course"]
+
+while "Python Crash Course" in books:
+    books.remove("Python Crash Course")
+
+print(books)
