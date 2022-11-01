766680922a3ef8f332067b5b1787edb64a396165
jsantore <jsantore@bridgew.edu>
2022-11-01 12:13:44 -0400

    breaking into functions

---
+++ FunctionMenu.py
@@ -0,0 +1,27 @@
+
+def get_data():
+    my_file = open('nationsPop.txt', 'r')
+    lines = my_file.readlines()
+    country_data = []
+    for country_line in lines:
+        country_line = country_line.strip()
+        nation_pop_list = country_line.split(',')
+        pop_dictionary = {'Name': nation_pop_list[0],
+                          'pop': int(nation_pop_list[1]),
+                          'change': float(nation_pop_list[2])
+                          }
+        country_data.append(pop_dictionary)
+    return country_data
+
+def print_menu():
+    print("========Please select from the following=========")
+    print("[1] Find the largest country")
+    print("[2] Find the smallest country")
+    print("[3] Add a new country")
+    print("[4] Quit")
+    print("=================================================")
+
+def main():
+    pass
+
+main()
