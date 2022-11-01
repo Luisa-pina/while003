60d66e5e1af45dad1be8e54e3a91412545151e71
jsantore <jsantore@bridgew.edu>
2022-11-01 11:43:34 -0400

    adding countries now

--- FileDataMenu.py
+++ FileDataMenu.py
@@ -26,6 +26,24 @@ while True:
         print("+++++++++++++++++")
         print(f"The largest country is {largest_so_far['Name']} with population {largest_so_far['pop']}")
     elif user_choice == '2':
-
+        smallest_so_far = country_data[0]
+        for current_country in country_data:
+            if current_country['pop'] < smallest_so_far['pop']:
+                smallest_so_far = current_country
+        print("--------------")
+        print(f"the smallest country is {smallest_so_far['Name']}")
+        print("----------------")
+    elif user_choice == '3':
+        new_name = input("What is the country name:")
+        new_pop = int(input(f"What is {new_name}'s population"))
+        new_change = float(input(f"What was the pop change for {new_name} for 2021-2022"))
+        new_data = {
+            'Name':new_name,
+            'pop':new_pop,
+            'change':new_change
+        }
+        country_data.append(new_data)
+    elif user_choice == '4':
+        exit(0)
     else:
         print("Please select 1-4")
