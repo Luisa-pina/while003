1f8837216917751e3e238a4ee1f60d776219f84b
jsantore <jsantore@bridgew.edu>
2022-11-08 11:30:49 -0500

    completed function based menu program

--- FunctionMenu.py
+++ FunctionMenu.py
@@ -29,14 +29,32 @@ def find_largest(country_data):
     print("+++++++++++++++++")
     print(f"The largest country is {largest_so_far['Name']} with population {largest_so_far['pop']}")
 
+def find_smallest(country_data):
+    smallest_so_far = country_data[0]
+    for current_country in country_data:
+        if current_country['pop'] < smallest_so_far['pop']:
+            smallest_so_far = current_country
+    print("--------------")
+    print(f"the smallest country is {smallest_so_far['Name']}")
+    print("----------------")
 
+def add_new_country(country_data):
+    new_name = input("What is the country name:")
+    new_pop = int(input(f"What is {new_name}'s population"))
+    new_change = float(input(f"What was the pop change for {new_name} for 2021-2022"))
+    new_data = {
+        'Name': new_name,
+        'pop': new_pop,
+        'change': new_change
+    }
+    country_data.append(new_data)
 def process_user_input(user_input, nations_pop):
     if user_input == '1':
         find_largest(nations_pop)
     elif user_input == '2':
-        pass
+        find_smallest(nations_pop)
     elif user_input == '3':
-        pass
+        add_new_country(nations_pop)
     elif user_input=='4':
         exit(0)
     else:
